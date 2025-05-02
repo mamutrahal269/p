cc4b8f02aeb3d8a345898c0864ec3f4f70d6a552
mamutrahal269 <levrymvc@gmail.com>
2025-05-02 13:46:06 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@ ostream &n(ostream &stream){
 	stream << "\n";
 	return stream;
 }
-void reader(ifstream &f){
+void read(ifstream &f){
 	char ch;
 	int size = 0;
 	cout << n;
 	while(f.get(ch)){
 		if(ch == '\\' && f.peek() == 'n'){
-			cout << "\n";
+			cout << n ;
 			f.get(ch);
 			continue;
 		}
@@ -27,23 +27,50 @@ void reader(ifstream &f){
 	cout << n << n << "Всего символов(включая невидимые) : " << size << n ;
 	f.close();
 }
-void writer(string s,ofstream &f){
+void write(string s,ofstream &f){
 	int i = 0;
 	while(s[i]) f.put(s[i++]);
 	f.close();
 }
-void copier(ofstream &to,ifstream &from){
+void copy(ofstream &to,ifstream &from){
 	char ch;
 	while(from.get(ch)) to.put(ch);
 	from.close();
 	to.close();
 }
+void paste(int byte,string str,string file){
+	ofstream w;
+	ifstream r;
+	r.open(file);
+	string temp_beg;
+	string temp_end;
+	char ch;
+	
+	for(int i = 0;i<byte && r;i++){
+		r.get(ch);
+		temp_beg += ch;
+	}
+	
+	for(;r;){
+		r.get(ch);
+		temp_end += ch;
+	}
+	r.close();
+	temp_beg += str + temp_end;
+	w.open(file,ios::out | ios::trunc | ios::binary);
+	int i = 0;
+	while(temp_beg[i]){
+		w.put(temp_beg[i]);
+		i++;
+	}
+	w.close();
+}
 void menu(){
-	cout << n << "Использование : \n \n./programm file.name чтение(1)/добавление(2)/перезапись(3)/\n  копирование(4)/изменение байта(5)" 
+	cout << n << "Использование : \n \n./programm file.name чтение(1)/добавление(2)/перезапись(3)/\n  копирование(4)/произвольная вставка(5)" 
 	<< n << n;
 }
 int main(int argc,char *argv[]){
-	if(argc < 3 || argc >= 4){
+	if(argc < 3 || argc > 3){
 		menu();
 		return 1;
 	}
@@ -54,19 +81,19 @@ int main(int argc,char *argv[]){
 			cout << "Не удалось открыть файл " << "\'" << argv[1] << "\'" << n;
 			return 1;
 		}
-		reader(file);
+		read(file);
 	}
 	if(atoi(argv[2]) == 2){
 		ofstream file(argv[1],ios::out | ios::app | ios::binary);
 		string str;
 		getline(cin,str);
-		writer(str,file);
+		write(str,file);
 	}
 	if(atoi(argv[2]) == 3){
 		ofstream file(argv[1],ios::out | ios::trunc);
 		string s;
 		getline(cin,s);
-		writer(s,file);
+		write(s,file);
 	}
 	if(atoi(argv[2]) == 4){
 		ifstream what(argv[1],ios::in | ios::binary);
@@ -77,7 +104,15 @@ int main(int argc,char *argv[]){
 		}
 		string s;
 		cin >> s;
-		ofstream where(s,ios::out | ios::trunc);
-		copier(where,what);
+		ofstream where(s,ios::out | ios::trunc | ios::binary);
+		copy(where,what);
+	}
+	if(atoi(argv[2]) == 5){
+		cout << n << "Введите байт и строку" << n;
+		string s;
+		int b;
+		cin >> b;
+		getline(cin,s);
+		paste(b,s,argv[1]);
 	}
 }
